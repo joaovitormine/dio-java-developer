6cedff39b71b8c38a62c542c901698e381a2ce00
João Vitor Mine Nascimento <joaovitormnascimento@yahoo.com>
2023-08-02 21:44:50 -0300

    Atualização do arquivo de estudo de variáveis Java

--- tipos-variaveis/src/TiposVariaveis.java
+++ tipos-variaveis/src/TiposVariaveis.java
@@ -1,5 +1,23 @@
 public class TiposVariaveis {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+		//tipos primitivos
+
+        String meuNome = "João Vitor";
+        
+        byte idade = 123;
+		short ano = 2021;
+		int cep = 21070333; // se começar com zero, talvez tenha que ser outro tipo
+		long cpf = 98765432109L; // se começar com zero, talvez tenha que ser outro tipo
+		float pi = 3.14F;
+		double salario = 1275.33;
+        
+        int numero = 5;
+
+        numero = 10;
+
+        System.out.println(numero);
+
+        final double VALOR_DE_PI = 3.14;
+    
     }
 }
