131702e99e996cc19fc01e32b07079568af93733
João Vitor Mine Nascimento <joaovitormnascimento@yahoo.com>
2023-08-09 00:33:42 -0300

    Estudos sobre Controle de Fluxo em Java

---
+++ controle-fluxo/src/ExemploExcecao.java
@@ -0,0 +1,18 @@
+import java.text.ParseException;
+import java.text.NumberFormat;
+
+public class ExemploExcecao {
+    public static void main(String[] args) {
+        Number valor;
+
+        try{
+
+            valor = NumberFormat.getInstance().parse("a1.75");
+            System.out.println(valor);
+
+        } catch(ParseException e ) {
+            e.printStackTrace();
+
+        }
+    }
+}
